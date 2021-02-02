3e82234ece64f19fff74b6d3df69b91ad0c2ed1f
anghelutar <anghelutar@yahoo.com>
2021-02-02 20:10:08 +0100

    Remove DE version.

--- alfresco-actuators/src/main/java/eu/xenit/actuators/webscripts/de/HealthV1Config.java
+++
@@ -1,8 +0,0 @@
-package eu.xenit.actuators.webscripts.de;
-
-public class HealthV1Config {
-    public static final String BASE_URL = "/xenit/api/v1/health/";
-    public static final String FAMILY = "Xenit-Health-v1";
-    public static final String FORMAT_JSON = "application/json";
-}
-
