899c2e131eaf2ab5be1ea68231da1569f415f74b
hechmi-dammak-xenit <107396375+hechmi-dammak-xenit@users.noreply.github.com>
2023-12-21 12:55:42 +0100

    XENOPS-1128 add check for content store fetching file to health check (#22)
    * XENOPS-1128 add check for content stora fetching file to health check
    
    * XENOPS-1128 add check for content stora fetching file to health check
    
    * XENOPS-1128 add check for content stora fetching file to health check
    
    * XENOPS-1128 add more testing for details
    
    * XENOPS-1128 change the way to access the noderef for content store to a constant

--- README.md
+++ README.md
@@ -4,38 +4,59 @@ Health endpoint, unauthenticated, to be used as docker and load balancer health 
 
 The webscript is available at:
 
+```
     alfresco/s/xenit/actuators/health
+    alfresco/s/xenit/actuators/health/details
+```
+
+with both having request parameter disabled with values :
+
+* `SystemInfoService`
+* `AlfrescoInfoService`
+* `ContentInfoService`
+* `LicenseInfoService`
+* `StatusInfoService`
+
+example:
+
+* `alfresco/s/xenit/actuators/health?disabled=SystemInfoService,LicenseInfoService`
+* `alfresco/s/xenit/actuators/health/details?disabled=SystemInfoService`
 
 ## Usage
 
 Status code is to be used for the health check.
 
-The output of the check is:
+The output of the health check is:
 
 ```
+
 {"status":"UP"}
+
 ```
 
 or
 
 ```
-{"status":"DOWN"}
+
+{"status":"DOWN" , "message":"error message"}
+
 ```
 
 Currently the check looks at:
 
 * system
-  * os
-  * java
-  * cpu
+    * os
+    * java
+    * cpu
 * alfresco
-  * id
-  * version
-  * edition
-  * license
-  * warManifest
-  * modules
-  * status
+    * id
+    * version
+    * edition
+    * license
+    * warManifest
+    * modules
+    * status
+    * content store
 
 # Development
 
@@ -46,5 +67,7 @@ See [TODO.md](TODO.md)
 ## How to run integration tests
 
 ```
+
 ./gradlew integrationTest
+
 ```
--- integration-tests/src/test/java/eu/xenit/actuators/integrationtesting/ActuatorsEndpointSmokeTest.java
+++ integration-tests/src/test/java/eu/xenit/actuators/integrationtesting/ActuatorsEndpointSmokeTest.java
@@ -1,23 +1,80 @@
 package eu.xenit.actuators.integrationtesting;
 
+import eu.xenit.actuators.model.gen.HealthInfo;
+import eu.xenit.actuators.model.gen.StatusEnum;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.containsString;
 
 
 class ActuatorsEndpointSmokeTest extends RestAssuredTest {
     @Test
     void testClassicalEndpoint() {
-        final String healthInfo = given()
+        final HealthInfo healthInfo = given()
                 .log().ifValidationFails()
                 .when()
                 .get("/s/xenit/actuators/health")
                 .then()
                 .log().ifValidationFails()
                 .statusCode(200)
-                .body(containsString("\"status\":\"UP\""))
-                .toString();
+                .extract().body().as(HealthInfo.class);
+        Assertions.assertEquals(StatusEnum.UP, healthInfo.getStatus());
     }
 
+    @Test
+    void testDetailsEndpoint() {
+        final ArrayList<HealthInfo> healthInfos = given()
+                .log().ifValidationFails()
+                .auth().preemptive().basic("admin", "admin")
+                .when()
+                .get("/s/xenit/actuators/health/details")
+                .then()
+                .log().ifValidationFails()
+                .statusCode(200)
+                .extract().body().as(HealthInfoList.class);
+        Assertions.assertTrue(healthInfos
+                .stream()
+                .map(HealthInfo::getName)
+                .collect(Collectors.toSet())
+                .containsAll(
+                        Set.of("SystemInfoService",
+                                "AlfrescoInfoService",
+                                "ContentInfoService",
+                                "LicenseInfoService",
+                                "StatusInfoService")));
+    }
+
+    @Test
+    void testDisabledDetailsEndpoint() {
+        final ArrayList<HealthInfo> healthInfos = given()
+                .log().ifValidationFails()
+                .auth().preemptive().basic("admin", "admin")
+                .when()
+                .get("/s/xenit/actuators/health/details?disabled=ContentInfoService,AlfrescoInfoService")
+                .then()
+                .log().ifValidationFails()
+                .statusCode(200)
+                .extract().body().as(HealthInfoList.class);
+        Set<String> names = healthInfos
+                .stream()
+                .map(HealthInfo::getName)
+                .collect(Collectors.toSet());
+        Assertions.assertTrue(names
+                .containsAll(
+                        Set.of("SystemInfoService",
+                                "LicenseInfoService",
+                                "StatusInfoService")));
+        Assertions.assertFalse(names.contains("AlfrescoInfoService"));
+        Assertions.assertFalse(names.contains("ContentInfoService"));
+
+    }
+
+    public static class HealthInfoList extends ArrayList<HealthInfo> {
+
+    }
 }
